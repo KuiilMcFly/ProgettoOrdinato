a4e5c2733c9c9f610390c087dc0783eb6bcaca79
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-05-25 12:47:18 +0200

    push

--- App.jsx
+++ App.jsx
@@ -10,39 +10,10 @@ function Home(props) {
   return (
     <View style={HomeStyles.container}>
       <View style={HomeStyles.header}>
-        
-      <View style={[HomeHeaderStyles.container, props.style]}>
-      <View style={HomeHeaderStyles.group}>
-        <Text style={HomeHeaderStyles.home}>HOME</Text>
-        <Image
-          source={require("../ProgettoSistemato/assets/HomeImg/settings.png")}
-          resizeMode="contain"
-          style={HomeHeaderStyles.image}
-        ></Image>
-      </View>
-    </View>
+        <Header />
       </View>
       <View style={HomeStyles.footer}>
-
-      <View style={[HomeFooterStyles, props.style]}>
-      <View style={HomeFooterStyles.group}>
-        <Image
-          source={require("../ProgettoSistemato/assets/HomeImg/bluetooth.png")}
-          resizeMode="contain"
-          style={HomeFooterStyles.image}
-        ></Image>
-        <Image
-          source={require("../ProgettoSistemato/assets/HomeImg/home.png")}
-          resizeMode="contain"
-          style={HomeFooterStyles.image2}
-        ></Image>
-        <Image
-          source={require("../ProgettoSistemato/assets/HomeImg/wifi.png")}
-          resizeMode="contain"
-          style={HomeFooterStyles.image3}
-        ></Image>
-      </View>
-    </View>
+        <Footer />
       </View>
     </View>
   );

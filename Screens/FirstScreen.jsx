63e7952bae08b3b20d3a4d243393790d64f0b474
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-05-29 10:44:29 +0200

    push

--- Screens/FirstScreen.jsx
+++ Screens/FirstScreen.jsx
@@ -9,25 +9,45 @@ function FirstScreen({navigation, ...props}) {
         <Text style={SettingCSS.lingua}>Lingua :</Text>
 
         <View style={SettingCSS.materialButtonViolet3}>
-          <TouchableOpacity style={[SettingCSS.container1, props.style]}>
+          <TouchableOpacity
+            onPress={() => {
+              //Logica per cambiare lingua
+              navigation.navigate('Home');
+            }}
+            style={[SettingCSS.container1, props.style]}>
             <Text style={SettingCSS.italiano}>Italiano</Text>
           </TouchableOpacity>
         </View>
 
         <View style={SettingCSS.materialButtonViolet4}>
-          <TouchableOpacity style={[SettingCSS.container2, props.style]}>
+          <TouchableOpacity
+            onPress={() => {
+              //Logica per cambiare lingua
+              navigation.navigate('Home');
+            }}
+            style={[SettingCSS.container2, props.style]}>
             <Text style={SettingCSS.english}>English</Text>
           </TouchableOpacity>
         </View>
 
         <View style={SettingCSS.materialButtonViolet5}>
-          <TouchableOpacity style={[SettingCSS.container3, props.style]}>
+          <TouchableOpacity
+            //Logica per cambiare lingua
+            onPress={() => {
+              navigation.navigate('Home');
+            }}
+            style={[SettingCSS.container3, props.style]}>
             <Text style={SettingCSS.francais}>Français</Text>
           </TouchableOpacity>
         </View>
 
         <View style={SettingCSS.materialButtonViolet6}>
-          <TouchableOpacity style={[SettingCSS.container4, props.style]}>
+          <TouchableOpacity
+            //Logica per cambiare lingua
+            onPress={() => {
+              navigation.navigate('Home');
+            }}
+            style={[SettingCSS.container4, props.style]}>
             <Text style={SettingCSS.deutsch}>Deutsch</Text>
           </TouchableOpacity>
         </View>

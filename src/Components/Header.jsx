6d67b9f372d93401126a21d9c96e6733098968b9
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-05-30 10:12:32 +0200

    pusho

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {HomeHeaderStyles} from '../Styles/HomeCSS/HomeHeaderStyles';
 import {View, Text, Image, Touchable, TouchableOpacity} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
 
 export const Header = ({navigation, ...props}) => {
   return (
-    <View style={[HomeHeaderStyles.container, props.style]}>
+    <View style={props.style}>
+      <LinearGradient
+        colors={['#5a80a6', '#404c94', '#3F51B5']}
+        style={HomeHeaderStyles.container}>
+
       <View style={HomeHeaderStyles.group}>
         <Text style={HomeHeaderStyles.home}>{props.title}</Text>
         <TouchableOpacity onPress={() => navigation.navigate('Setting')}>
@@ -14,6 +20,7 @@ export const Header = ({navigation, ...props}) => {
             style={HomeHeaderStyles.image}></Image>
         </TouchableOpacity>
       </View>
+            </LinearGradient>
     </View>
   );
 };

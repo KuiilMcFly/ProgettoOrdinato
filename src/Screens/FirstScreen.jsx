f4cb189fe07ae3582227692c26f5f562de329961
Vincenzo928el <98588521+Vincenzo928el@users.noreply.github.com>
2023-05-30 15:32:27 +0200

    fix-screen

--- src/Screens/FirstScreen.jsx
+++ src/Screens/FirstScreen.jsx
@@ -1,79 +1,105 @@
-import React from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
-import {SettingCSS} from '../Styles/SettingCSS/SettingCSS';
-import {useNavigation} from '@react-navigation/native';
-import {useTranslation} from 'react-i18next';
+import React, { useState, useEffect } from 'react';
+import { View, TouchableOpacity, Text, Image, ActivityIndicator } from 'react-native';
+import { SettingCSS } from '../Styles/SettingCSS/SettingCSS';
+import { useNavigation } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
 import LinearGradient from 'react-native-linear-gradient';
-function FirstScreen({onLanguageSelected, ...props}) {
-  const {i18n} = useTranslation();
+
+// Importa l'immagine di caricamento
+import LoadingImage from '../assets/Bluetooth.png';
+
+function FirstScreen({ onLanguageSelected, ...props }) {
+  const { i18n } = useTranslation();
+  const [loading, setLoading] = useState(false); // Imposta lo stato di caricamento su false all'inizio
 
   const changeLanguageHandler = language => {
+    setLoading(true); // Imposta lo stato di caricamento su true immediatamente
+
     i18n.changeLanguage(language);
-    onLanguageSelected();
+    setTimeout(() => {
+      setLoading(false); // Imposta lo stato di caricamento su false dopo 5 secondi
+      onLanguageSelected();
+    }, 2000); // Ritardo di 5 secondi (5000 millisecondi)
   };
 
   const navigation = useNavigation();
   return (
     <View>
+      {loading ? (
+        <LinearGradient  colors={['#82c0d1', '#508796', '#d7d8db']}
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
+        }}>
+          <Image source={LoadingImage} style={{ width: 500, height: 500}} />
+          <ActivityIndicator size="large" color="#000000" />
+          <Text>Loading...</Text>
+        </LinearGradient>
+      ) : (
         <LinearGradient
-      colors={['#82c0d1', '#508796', '#d7d8db']}
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-      }}
-      >
+          colors={['#82c0d1', '#508796', '#d7d8db']}
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <View style={SettingCSS.textBtn}>
+            <Text style={SettingCSS.lingua}>Lingua :</Text>
 
-      <View style={SettingCSS.textBtn}>
-        <Text style={SettingCSS.lingua}>Lingua :</Text>
+            <View style={SettingCSS.materialButtonViolet3}>
+              <TouchableOpacity
+                onPress={() => {
+                  changeLanguageHandler('it');
+                  setLoading(true); // Imposta lo stato di caricamento su true
+                }}
+                style={[SettingCSS.container1, props.style]}
+              >
+                <Text style={SettingCSS.italiano}>IT 🇮🇹</Text>
+              </TouchableOpacity>
+            </View>
 
-        <View style={SettingCSS.materialButtonViolet3}>
-          <TouchableOpacity
-            onPress={() => {
-              changeLanguageHandler('it');
-              onLanguageSelected();
-            }}
-            style={[SettingCSS.container1, props.style]}>
-            <Text style={SettingCSS.italiano}>IT 🇮🇹</Text>
-          </TouchableOpacity>
-        </View>
+            <View style={SettingCSS.materialButtonViolet4}>
+              <TouchableOpacity
+                onPress={() => {
+                  changeLanguageHandler('en');
+                  setLoading(true); // Imposta lo stato di caricamento su true
+                }}
+                style={[SettingCSS.container2, props.style]}
+              >
+                <Text style={SettingCSS.english}>EN 🇬🇧</Text>
+              </TouchableOpacity>
+            </View>
 
-        <View style={SettingCSS.materialButtonViolet4}>
-          <TouchableOpacity
-            onPress={() => {
-              changeLanguageHandler('en');
-              onLanguageSelected();
-            }}
-            style={[SettingCSS.container2, props.style]}>
-            <Text style={SettingCSS.english}>EN 🇬🇧</Text>
-          </TouchableOpacity>
-        </View>
+            <View style={SettingCSS.materialButtonViolet5}>
+              <TouchableOpacity
+                onPress={() => {
+                  changeLanguageHandler('fr');
+                  setLoading(true); // Imposta lo stato di caricamento su true
+                }}
+                style={[SettingCSS.container3, props.style]}
+              >
+                <Text style={SettingCSS.francais}>FR 🇫🇷</Text>
+              </TouchableOpacity>
+            </View>
 
-        <View style={SettingCSS.materialButtonViolet5}>
-          <TouchableOpacity
-            onPress={() => {
-              changeLanguageHandler('fr');
-              onLanguageSelected();
-            }}
-            style={[SettingCSS.container3, props.style]}>
-            <Text style={SettingCSS.francais}>FR 🇫🇷</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={SettingCSS.materialButtonViolet6}>
-          <TouchableOpacity
-            //Logica per cambiare lingua
-            onPress={() => {
-              changeLanguageHandler('de');
-              onLanguageSelected();
-            }}
-            style={[SettingCSS.container4, props.style]}>
-            <Text style={SettingCSS.deutsch}>DE 🇩🇪</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-              </LinearGradient>
+            <View style={SettingCSS.materialButtonViolet6}>
+              <TouchableOpacity
+                onPress={() => {
+                  changeLanguageHandler('de');
+                  setLoading(true); // Imposta lo stato di caricamento su true
+                }}
+                style={[SettingCSS.container4, props.style]}
+              >
+                <Text style={SettingCSS.deutsch}>DE 🇩🇪</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </LinearGradient>
+      )}
     </View>
   );
 }

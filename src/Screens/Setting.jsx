9bb8514a8ba21001e6660aa54419c647e5a66111
Vincenzo928el <98588521+Vincenzo928el@users.noreply.github.com>
2023-06-08 09:40:50 +0200

    aggiunta overlay

--- src/Screens/Setting.jsx
+++ src/Screens/Setting.jsx
@@ -1,14 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import {View, TouchableOpacity, Text, Alert, Image} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TouchableOpacity, Text, Alert, Image, StyleSheet } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
-import {SettingCSS} from '../Styles/SettingCSS/SettingCSS';
-import {useTranslation} from 'react-i18next';
+import { SettingCSS } from '../Styles/SettingCSS/SettingCSS';
+import { useTranslation } from 'react-i18next';
 import LinearGradient from 'react-native-linear-gradient';
-import {HomeHeaderStyles} from '../Styles/HomeCSS/HomeHeaderStyles';
+import { HomeHeaderStyles } from '../Styles/HomeCSS/HomeHeaderStyles';
 
-function Setting({navigation, bluetoothConnection = false, ...props}) {
+function Setting({ navigation, bluetoothConnection = false, ...props }) {
   const [spinner, setSpinner] = useState(false);
-  const {i18n} = useTranslation();
+  const { i18n } = useTranslation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isMenuVisible, setIsMenuVisible] = useState(false);
 
@@ -46,7 +46,8 @@ function Setting({navigation, bluetoothConnection = false, ...props}) {
   return (
     <LinearGradient
       colors={['#82c0d1', '#508796', '#d7d8db']}
-      style={SettingCSS.container}>
+      style={SettingCSS.container}
+    >
       <View style={HomeHeaderStyles.group}>
         {isMenuOpen ? (
           <TouchableOpacity onPress={toggleMenu}>
@@ -71,7 +72,7 @@ function Setting({navigation, bluetoothConnection = false, ...props}) {
       <Spinner
         visible={spinner}
         textContent={'Loading...'}
-        textStyle={{color: '#FFF'}}
+        textStyle={{ color: '#FFF' }}
       />
       <View style={SettingCSS.textBtn}>
         <Text style={SettingCSS.lingua}>{i18n.t('lingua')}:</Text>
@@ -81,7 +82,8 @@ function Setting({navigation, bluetoothConnection = false, ...props}) {
             onPress={() => {
               changeLanguageHandler('it');
             }}
-            style={[SettingCSS.container1, props.style]}>
+            style={[SettingCSS.container1, props.style]}
+          >
             <Text style={SettingCSS.italiano}>Italiano 🇮🇹</Text>
           </TouchableOpacity>
         </View>
@@ -91,7 +93,8 @@ function Setting({navigation, bluetoothConnection = false, ...props}) {
             onPress={() => {
               changeLanguageHandler('en');
             }}
-            style={[SettingCSS.container2, props.style]}>
+            style={[SettingCSS.container2, props.style]}
+          >
             <Text style={SettingCSS.english}>English 🇬🇧</Text>
           </TouchableOpacity>
         </View>
@@ -101,7 +104,8 @@ function Setting({navigation, bluetoothConnection = false, ...props}) {
             onPress={() => {
               changeLanguageHandler('fr');
             }}
-            style={[SettingCSS.container3, props.style]}>
+            style={[SettingCSS.container3, props.style]}
+          >
             <Text style={SettingCSS.francais}>Français 🇫🇷</Text>
           </TouchableOpacity>
         </View>
@@ -111,63 +115,64 @@ function Setting({navigation, bluetoothConnection = false, ...props}) {
             onPress={() => {
               changeLanguageHandler('de');
             }}
-            style={[SettingCSS.container4, props.style]}>
+            style={[SettingCSS.container4, props.style]}
+          >
             <Text style={SettingCSS.deutsch}>Deutsch 🇩🇪</Text>
           </TouchableOpacity>
         </View>
       </View>
 
       {isMenuOpen && (
+        <View style={styles.overlay}>
           <LinearGradient
             style={HomeHeaderStyles.hamburgerMenu}
-            colors={['#82c0d1', '#508796', '#d7d8db']}>
-            {isMenuOpen ? (
-              <TouchableOpacity onPress={toggleMenu}>
-                <View style = {{backgroundColor: '#3F51B5', height: 40}}>
+            colors={['#82c0d1', '#508796', '#d7d8db']}
+          >
+            <TouchableOpacity onPress={toggleMenu}>
+              <View style={{ backgroundColor: '#3F51B5', height: 40 }}>
                 <Image
                   source={require('../assets/HomeImg/close.png')}
                   resizeMode="contain"
                   style={HomeHeaderStyles.menuIcon2}
                 />
-                </View>
-              </TouchableOpacity>
-            ) : (
-              <TouchableOpacity onPress={toggleMenu}>
-                <Image
-                  source={require('../assets/HomeImg/menu.png')}
-                  resizeMode="contain"
-                  style={HomeHeaderStyles.menuIcon}
-                />
-              </TouchableOpacity>
-            )}
-          <View style={{width: '100%', height: '35%', marginLeft: '0%'}}>
-            <Image
-              source={require('../assets/ble.png')}
-              style={{width: '100%', height: '100%'}}
-            />
-          </View>
-          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-            <Text style={HomeHeaderStyles.menuItem}>HOME</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Bluetooth')}>
-            <Text style={HomeHeaderStyles.menuItem}>BLUETOOTH</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => checkBluetooth()}>
-            <Text style={HomeHeaderStyles.menuItem}>WIFI</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Historical')}>
+              </View>
+            </TouchableOpacity>
+            <View style={{ width: '100%', height: '35%', marginLeft: '0%' }}>
+              <Image
+                source={require('../assets/ble.png')}
+                style={{ width: '100%', height: '100%' }}
+              />
+            </View>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text style={HomeHeaderStyles.menuItem}>HOME</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Bluetooth')}>
+              <Text style={HomeHeaderStyles.menuItem}>BLUETOOTH</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => checkBluetooth()}>
+              <Text style={HomeHeaderStyles.menuItem}>WIFI</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Historical')}>
               <Text style={HomeHeaderStyles.menuItem}>{i18n.t('historical')}</Text>
             </TouchableOpacity>
             <TouchableOpacity>
               <Text style={HomeHeaderStyles.menuItem}>{i18n.t('contact')}</Text>
             </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('AboutUs')}>
-            <Text style={HomeHeaderStyles.menuItem}>{i18n.t('about')} </Text>
-          </TouchableOpacity>
-        </LinearGradient>
+            <TouchableOpacity onPress={() => navigation.navigate('AboutUs')}>
+              <Text style={HomeHeaderStyles.menuItem}>{i18n.t('about')} </Text>
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
       )}
     </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Imposta l'opacità qui
+  },
+});
 
 export default Setting;
